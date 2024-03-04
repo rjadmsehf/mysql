2242efb7b001176a78fd1579e83a07579df566eb
rjadmsehf <jjg524@naver.com>
2024-03-04 12:10:25 +0900

    20240304

---
+++ 08-subquery.sql
@@ -0,0 +1,101 @@
+-- subqueries
+-- 다른 쿼리 내에서 실행되는 쿼리이다.
+-- subqueries 의 결과를 활용하여 복잡한 mainquery 를 작성해 한 번에 
+-- 여러 작업을 수행할 수 있다.
+
+-- tbl_menu 테이블에서 민트미역국과 같은 카테고리 코드를 가지고 있는 행들의 모든 
+-- 정보를 조회하시오.
+
+select
+	*
+from
+	tbl_menu
+where
+	menu_name = '민트미역국';
+    
+select * from tbl_menu where category_code = 4;
+
+-- 해결
+
+select * from tbl_menu where category_code = ( select category_code from tbl_menu where menu_name = '민트미역국'); 
+	
+-- count() : 특정 열 또는 행의 수를 반환하는 데 사용
+select category_code , count(*) as 'count' from tbl_menu group by category_code;
+
+-- 비교를 위한 추가작성분 (내가씀)
+select * from tbl_menu;
+
+-- from 절에 쓰인 서브쿼리(derived table, *파생 테이블* )
+-- 반드시 자신의 별칭이 있어야 한다.
+-- 이 count 라는 컬럼이 가진 가장 큰값 -> 6 이라는 값을 도출하고 싶다.
+
+-- max() : 최댓값을 반환해줌.
+
+select  max(count)  
+from  (select count(*) 
+as 'count' 
+from tbl_menu 
+group by category_code) 
+as countmenu ;    -- 명명규칙 없으면 실행이 안됌
+
+-- 상관 서브쿼리
+-- 메인쿼리가 서브쿼리 결과에 영향을 주는 경우 상관 서브쿼리 라고 한다.
+
+-- tbl_menu 테이블에서 카테고리 코드가 4번인 친구들의 메뉴 가격 평균을 조회하시오 
+-- avg()
+
+select avg(menu_price) from tbl_menu where category_code = 4;
+
+-- 카테고리별 평균 가격보다 높은 가격의 메뉴의 메뉴코드, 메뉴이름 , 메뉴가격 ,카테고리코드 를 조회하세염  (잘안씀)
+
+SELECT 
+    a.menu_code, 
+    a.menu_name, 
+    a.menu_price, 
+    a.category_code
+FROM 
+    tbl_menu a
+WHERE 
+    menu_price > (
+        SELECT 
+            AVG(menu_price)
+        FROM 
+            tbl_menu 
+        WHERE 
+            category_code = a.category_code
+    );
+-- cte(common table expressions)
+-- 파생 테이블과 비슷한 개념이며 코드의 가독성과 재사용성을 위해 파생 테이블 
+-- 대신 사용하게 된다.
+
+
+select	
+	a.menu_name,
+    b.category_name
+from 
+	tbl_menu a
+join 
+	tbl_category b   
+	on 
+    a.category_code = b.category_code;
+
+with menucate as (
+
+				select	
+					a.menu_name,
+					b.category_name
+				from 
+					tbl_menu a
+				join 
+					tbl_category b   
+				on 
+					a.category_code = b.category_code
+					
+		  		 )
+
+select
+	*
+from	
+	menucate;
+
+    
---
+++ 09-set_opertors.sql
@@ -0,0 +1,78 @@
+-- 09_set_operators
+
+-- 두 개 이상의 select 문의 결과 집합을 결합하는데 사용한다.
+
+-- union 두 원의 겹치는 부분 제거 
+-- 두 개 이상의 select 문의 결과를 결합하여 중복된 레코드를 제거한 후 
+-- 반환하는 sql 연산자 이다.
+
+-- 1 , A
+select* from tbl_menu where category_code = 10
+
+union
+
+-- 2 , B
+select* from tbl_menu where menu_price < 9000;
+
+
+-- union all 두 원과 겹치는 부분 포함
+-- 두 개 이상의 select 문의 결과를 결합하여 중복된 레코드를 제거하지 않고 모두 반환 
+
+-- 1 , A
+select* from tbl_menu where category_code = 10
+
+union all
+
+-- 2 , B
+select * from tbl_menu where menu_price < 9000;
+
+-- intersect
+-- 두 select 문의 결과 중 공통되는 레코드만 반환하는 sql 연산자이다.
+-- mysql 은 intersect 제공하지 않는다. 
+-- 하지만 inner join 또는 in 연산자를 활용해서 구현하는 것은 가능하다.
+
+-- inner join 활용 
+-- 1 , A
+select * from tbl_menu;
+
+-- 2 , B
+select 
+	* 
+from 
+	tbl_menu
+where 
+	menu_price < 9000;
+-- 결과    
+
+select 
+	a.menu_code,
+    a.menu_name,
+    a.menu_price,
+    a.category_code,
+    a.orderable_status
+from 
+	tbl_menu a
+join 
+	(
+    select 
+	menu_code,
+    menu_name,
+    menu_price,
+    category_code,
+    orderable_status
+	from 
+	tbl_menu 
+	where
+	menu_price < 9000
+    )b
+on
+(a.menu_code = b.menu_code);
+    
+    
+    
+    
+    
+    
+    
+    
+    
